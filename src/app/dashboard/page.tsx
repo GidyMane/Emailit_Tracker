e9f0b11f29295fd6fed4250f413bfb38e9dd0df2
GidyMane <muragegideon2000@gmail.com>
2025-08-05 21:06:32 +0300

    update

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+            Dashboard
+        </h1>
+        <p className="text-lg text-gray-600 mb-8">
+            Welcome to your dashboard!
+        </p>
+        <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
+            Dashboard will be here soon!
+        </div>      
+    </div>
+  )
+}
+
+export default page
