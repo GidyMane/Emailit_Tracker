62811fad56317b7f7436e235da242f169a9d4dc8
GidyMane <muragegideon2000@gmail.com>
2025-08-06 10:31:14 +0300

    store user data

---
+++ src/app/utils/constants.tsx
@@ -0,0 +1,8 @@
+
+// export const baseUrl= "http://localhost:3000/api/v1/"
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
+export const ROUTES = {
+  SAVE_USER: "/api/auth/kindecallback",
+};
+ 
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,26 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import type { NextRequest } from "next/server";
+import axios from "axios";
+import { BASE_URL, ROUTES } from "@/app/utils/constants";
+
 
 export default withAuth(
-  async function middleware(req: any) {
+  async function middleware(req: NextRequest) {
+    try {
+      await axios.get(`${BASE_URL}${ROUTES.SAVE_USER}`, {
+        headers: {
+          Cookie: req.headers.get("cookie") || "",
+        },
+      });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error("Failed to sync user:", error.message);
+      } else {
+        console.error("Unknown error occurred during user sync.");
+      }
+    }
   },
   {
-    // Middleware still runs on all routes, but doesn't protect the blog route
     publicPaths: ["/blog"],
   }
 );
@@ -13,4 +29,4 @@ export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
   ],
-}
+};
